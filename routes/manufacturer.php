69d1bf0d2f74c4fa074194f9f6b2e7caf8e4969a
Vítor Soares Vian <vitorsoaresvian@gmail.com>
2019-07-01 21:21:36 -0300

    Creating the route all of manufacturers in route file

--- routes/manufacturer.php
+++ routes/manufacturer.php
@@ -4,6 +4,7 @@ Route::group(['prefix' => 'manufacturer', 'name' => 'manufacturer.'], function()
     Route::get('{skip}/{take}/index', 'ManufacturersController@index')->name('index');
     Route::post('store', 'ManufacturersController@store')->name('store');
     Route::get('{id}/show', 'ManufacturersController@show')->name('show');
+    Route::get('all', 'ManufacturersController@all')->name('all');
     Route::get('count', 'ManufacturersController@count')->name('count');
     Route::post('{id}/update', 'ManufacturersController@update')->name('update');
     Route::get('{id}/destroy', 'ManufacturersController@destroy')->name('destroy');
