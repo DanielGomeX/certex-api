897ebb96cd9c0bdbbc4251f24c46862b0f3ace73
Vítor Soares Vian <vitorsoaresvian@gmail.com>
2019-07-05 19:46:54 -0300

    Certification route show added

--- routes/certification.php
+++ routes/certification.php
@@ -6,6 +6,6 @@ Route::group(['prefix' => 'certification', 'name' => 'certification.'], function
     Route::post('store', 'CertificationsController@store')->name('store');
     Route::post('{id}/update', 'CertificationsController@update')->name('update');
     Route::get('{id}/destroy', 'CertificationsController@destroy')->name('destroy');
-    // Route::get('{id}/show', 'CertificationsController@show')->name('show');
+    Route::get('{id}/show', 'CertificationsController@show')->name('show');
     Route::get('{certificationId}/{extinguisherId}/generate', 'CertificationsController@generate')->name('generate');
 });
