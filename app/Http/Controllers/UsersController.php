36129aefd5ede7158230aac1f6d31d30ef1588e8
Vítor Soares Vian <vitorsoaresvian@gmail.com>
2019-07-05 20:11:38 -0300

    users

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\User;
+use Validator;
+use App\Helpers\APIHelper;
 
 class UsersController extends Controller
 {
