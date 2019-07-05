8e6b2b3e851b9e445c6f6f819a4bce47c204590f
Vítor Soares Vian <vitorsoaresvian@gmail.com>
2019-07-05 20:24:05 -0300

    user store

--- routes/user.php
+++ routes/user.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::group(['prefix' => 'user', 'name' => 'user.'], function() {
-    Route::get('store', 'UsersController@store')->name('store');
     Route::get('{id}/show', 'UsersController@show')->name('show');
+    Route::post('store', 'UsersController@store')->name('store');
     Route::post('{id}/update', 'UsersController@update')->name('update');
 });
